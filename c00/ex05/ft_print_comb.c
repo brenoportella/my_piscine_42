2ac0a307c98ec5e6891d4bd3a0ff4624fb0fab24
Breno Portella De Andrade Bohry <bportell@c1r4s2.42lisboa.com>
2024-08-21 15:13:45 +0100

    overwrite with my 42repository changes

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 15:51:59 by bportell          #+#    #+#             */
+/*   Updated: 2024/08/21 13:43:30 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
 	char	first;
 	char	second;
@@ -18,10 +30,8 @@ void ft_print_comb(void)
 				write (1, &first, 1);
 				write (1, &second, 1);
 				write (1, &third, 1);
-				if (first != '7' || second != '8'|| third != '9')
-				{
+				if (first != '7' || second != '8' || third != '9')
 					write (1, ", ", 2);
-				}
 				third++;
 			}
 			second++;
--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -1,42 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 13:51:33 by bportell          #+#    #+#             */
+/*   Updated: 2024/08/21 15:11:20 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_print_comb2(void)
 {
-	char	c1;
-	char	c2;
-	char	c3;
-	char	c4;
+	char	nbrs [6];
+	int		left_n;
+	int		right_n;
 
-	c1 = '0';
-	while (c1 <= '9')
+	left_n = 0;
+	while (left_n <= 98)
 	{
-		c2 = '0';
-		while (c2 <= '9')
+		right_n = left_n + 1;
+		while (right_n <= 99)
 		{
-			c3 = c1;
-			while (c3 <= '9')
-			{
-				c4 = c2 + 1;
-				while (c4 <= '9')
-				{
-					write (1, &c1, 1);
-					write (1, &c2, 1);
-					write (1, " ", 1);
-					write (1, &c3, 1);
-					write (1, &c4, 1);
-					write (1, ", ", 2);
-					c4++;
-				}
-				c3++;
-			}
-			c2++;
+			nbrs[0] = '0' + left_n / 10;
+			nbrs[1] = '0' + left_n % 10;
+			nbrs[2] = ' ';
+			nbrs[3] = '0' + right_n / 10;
+			nbrs[4] = '0' + right_n % 10;
+			write (1, nbrs, 5);
+			if (left_n != 98 || right_n != 99)
+				write (1, ", ", 2);
+			right_n++;
 		}
-		c1++;
-	}		
+		left_n++;
+	}
 	write (1, "\n", 1);
-}
-
-int	main(void)
-{
-	ft_print_comb2();
 }
