c7a59331a0f72c59bd647a4da038e1ca9abe7ad5
Breno Portella De Andrade Bohry <bportell@c1r4s2.42lisboa.com>
2024-08-21 13:33:19 +0100

    add c01 ex00 and ex01

---
+++ c01/ex00/ft_ft.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 19:09:36 by bportell          #+#    #+#             */
+/*   Updated: 2024/08/20 20:27:14 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*int	main()
+{
+	int	valor = 0; //declaracao de inteiro
+
+	printf("%d", valor); //exibicao do inteiro
+	printf(" agora mudamos o valor ");
+	ft_ft(&valor); //agora passamos o inteiro como pointer para a funcao
+		       //e a funcao atribui 42 para o inteiro
+	printf("%d", valor);//exibimos o novo valor do inteiro agora 42
+	
+	return 0;
+}*/
