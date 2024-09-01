37e085ff0900ea73211377066e59a14d2171ed64
Breno Portella De Andrade Bohry <bportell@c1r8s11.42lisboa.com>
2024-09-01 22:12:16 +0100

    add ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: bportell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 20:08:04 by bportell          #+#    #+#             */
-/*   Updated: 2024/09/01 20:33:50 by bportell         ###   ########.fr       */
+/*   Updated: 2024/09/01 22:12:03 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	ft_strlen(char *str)
 {
 	int	i;
 
-	while(str[i] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
 		i++;
 	}
@@ -26,24 +27,26 @@ int	ft_strlen(char *str)
 
 int	ft_alphanumeric(char p)
 {
-	if(!((p >= 48 && p <= 57) || (p >= 65 && p <= 90) || (p >= 97 && p <= 122)))
-		return (0)
+	if (!((p >= '0' && p <= '9')
+			|| (p >= 'A' && p <= 'Z')
+			|| (p >= 'a' && p <= 'z')))
+		return (0);
 	else
-		return (1)
+		return (1);
 }
 
-char    *ft_strlowcase(char *str)
+char	*ft_strlowcase(char *str)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        while (str[i] != '\0')
-        {
-                if (str[i] >= 65 && str[i] <= 90)
-                        str[i] = str[i]+32;
-                i++;
-        }
-        return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
 }
 
 char	*ft_strcapitalize(char *str)
@@ -52,19 +55,25 @@ char	*ft_strcapitalize(char *str)
 
 	i = ft_strlen(str);
 	ft_strlowcase(str);
-
 	while (i >= 0)
 	{
-	
+		if (str[i] == 0 && ft_alphanumeric(str[i]))
+		{
+			str[i] -= 32;
+		}
+		if (!ft_alphanumeric(str[i - 1]) && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i--;
 	}
+	return (str);
 }
+/*
+int	main(void)
+{
+	char	str[] = "ola, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
 
-
-
-
-
-
-
-
-
-
+	ft_strcapitalize(str);
+	printf("%s\n", str);
+}*/
