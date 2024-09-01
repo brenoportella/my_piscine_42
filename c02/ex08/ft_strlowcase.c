41d63e1a2b3bff421c7a802b986e58e04143a52b
Breno Portella De Andrade Bohry <bportell@c1r8s11.42lisboa.com>
2024-09-01 12:44:29 +0100

    add ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 12:40:16 by bportell          #+#    #+#             */
+/*   Updated: 2024/09/01 12:44:16 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strlowcase(char *str);
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] = str[i]+32;
+		i++;
+	}
+	return (str);
+}
+/*
+int	main(void)
+{
+	char	str_upper [] = "brEno gg";
+
+	ft_strlowcase(str_upper);
+
+	printf("%s\n", str_upper);
+}*/
