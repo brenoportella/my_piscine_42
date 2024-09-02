bc53fc37a9407563783d38a44b71d987c90034ab
brenoportella <bp.bohry@gmail.com>
2024-09-02 10:48:11 +0100

    add ex03

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,30 @@
+
+//#include <stdio.h>
+//#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	str1[20] = "breno ";
+	char	str2[] = "gg";
+
+	printf("%s\n", ft_strncat(str1, str2, 1));
+	return (0);
+}*/
